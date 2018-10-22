5d1d794f96d03ac77d28d7ce74e958a0f3596675
nicozorza <tecnico2011>
2018-10-22 00:19:06 -0300

    Add plots. Add wav saving.

--- src/train_encoder_decoder.py
+++ src/train_encoder_decoder.py
@@ -1,11 +1,16 @@
 import tensorflow as tf
-
+import numpy as np
 from src.neural_network.EncoderDecoder.EncoderDecoder import EncoderDecoder
 from src.neural_network.EncoderDecoder.EncoderDecoderData import EncoderDecoderData
 from src.utils.Database import Database
 from src.utils.ProjectData import ProjectData
 import matplotlib.pyplot as plt
+import scipy.io.wavfile as wav
+from scipy import signal
 
+
+show_plots = True
+save_wav = False
 
 ##########################################################################################################
 # Load project data
@@ -20,15 +25,15 @@ network_data.tensorboard_path = project_data.ENC_DEC_TENSORBOARD_PATH
 network_data.input_features = 513
 
 network_data.encoder_num_layers = 3
-network_data.encoder_num_units = [256, 128, 64, 32, 32]
-network_data.encoder_activation = [None]*2 + [tf.nn.tanh] * 3
+network_data.encoder_num_units = [256, 128, 64]
+network_data.encoder_activation = [None, tf.nn.sigmoid, tf.nn.sigmoid]
 network_data.encoder_regularizer = 0.2
 network_data.encoder_batch_norm = False
 network_data.encoder_keep_prob = [0.8] * network_data.encoder_num_layers
 
-network_data.decoder_num_layers = 5
-network_data.decoder_num_units = [32, 32, 64, 128, 256]
-network_data.decoder_activation = [tf.nn.tanh] * 3 + [None]*2
+network_data.decoder_num_layers = 3
+network_data.decoder_num_units = [64, 128, 256]
+network_data.decoder_activation = [tf.nn.sigmoid, tf.nn.sigmoid, None]
 network_data.decoder_regularizer = 0.2
 network_data.decoder_batch_norm = False
 network_data.decoder_keep_prob = [0.8] * network_data.decoder_num_layers
@@ -47,32 +52,52 @@ encoder_decoder = EncoderDecoder(network_data)
 
 encoder_decoder.create_graph()
 
+train_database = Database.fromFile(project_data.TRAIN_ENCODER_DATABASE_FILE, project_data)
+train_feats, _ = train_database.to_set()
+# train_feats = train_feats[0:1]
 test_database = Database.fromFile(project_data.TEST_ENCODER_DATABASE_FILE, project_data)
 test_feats, _ = test_database.to_set()
-test_feats = test_feats[0:100]
 
 encoder_decoder.train(
-    input_seq=test_feats,
-    output_seq=test_feats,
+    input_seq=train_feats,
+    output_seq=train_feats,
     restore_run=True,
     save_partial=True,
     save_freq=10,
     use_tensorboard=True,
     tensorboard_freq=5,
-    training_epochs=1,
+    training_epochs=10,
     batch_size=10
 )
 
-out = encoder_decoder.predict(test_feats[0])
-f1 = plt.figure(0)
-plt.imshow(out, cmap='hot', interpolation='nearest')
-f2 = plt.figure(1)
-plt.imshow(test_feats[0], cmap='hot', interpolation='nearest')
+encoder_decoder.validate(test_feats, test_feats, True, 10)
 
-# out = encoder_decoder.predict(test_feats[1])
-# f3 = plt.figure(2)
-# plt.imshow(out, cmap='hot', interpolation='nearest')
-# f4 = plt.figure(3)
-# plt.imshow(test_feats[1], cmap='hot', interpolation='nearest')
+if show_plots:
+    out = encoder_decoder.predict(test_feats[0])
+    f1 = plt.figure(0)
+    plt.imshow(out, cmap='hot', interpolation='nearest')
+    f2 = plt.figure(1)
+    plt.imshow(test_feats[0], cmap='hot', interpolation='nearest')
 
-plt.show()
+    plt.show()
+
+
+if save_wav:
+    fs = int(test_database[0].item_feature.fs)
+    winlen = 20
+    winstride = 10
+    nperseg = int(round(winlen * fs / 1e3))
+    noverlap = int(round(winstride * fs / 1e3))
+
+    _, xrec = signal.istft(np.exp(test_feats[0].transpose()), fs=fs,
+                           window='hann', nperseg=nperseg, noverlap=noverlap, nfft=1024)
+    xrec = xrec - min(xrec)
+    xrec = xrec / abs(max(xrec))
+    wav.write('/home/nicozorza/Escritorio/asd1.wav', fs, xrec)
+
+    out = encoder_decoder.predict(test_feats[0])
+    _, xrec = signal.istft(np.exp(out.transpose()), fs=fs,
+                           window='hann', nperseg=nperseg, noverlap=noverlap, nfft=513)
+    xrec = xrec - min(xrec)
+    xrec = xrec / abs(max(xrec))
+    wav.write('/home/nicozorza/Escritorio/asd2.wav', fs, xrec)
