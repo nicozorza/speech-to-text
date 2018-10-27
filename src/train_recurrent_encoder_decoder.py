b186901175ac2e4379cbb0c3f22a9053681a7721
nicozorza <tecnico2011>
2018-10-27 19:44:20 -0300

    Update parameters

--- src/train_recurrent_encoder_decoder.py
+++ src/train_recurrent_encoder_decoder.py
@@ -19,19 +19,19 @@ network_data.tensorboard_path = project_data.ENC_DEC_TENSORBOARD_PATH
 network_data.input_features = 513
 
 network_data.num_encoder_layers = 2
-network_data.num_encoder_bw_units = [128, 64]
-network_data.num_encoder_fw_units = [128, 64]
-network_data.encoder_activation = None#[tf.nn.tanh] * 2
+network_data.num_encoder_bw_units = [128, 128]
+network_data.num_encoder_fw_units = [128, 128]
+network_data.encoder_activation = [tf.nn.sigmoid, tf.nn.sigmoid]
 network_data.encoder_regularizer = 0.2
-network_data.encoder_output_sizes = [200, 80]
-network_data.encoder_out_activation = None
+network_data.encoder_output_sizes = [256, 128]
+network_data.encoder_out_activation = tf.nn.sigmoid
 
 network_data.num_decoder_layers = 2
-network_data.num_decoder_bw_units = [128, 64]
-network_data.num_decoder_fw_units = [128, 64]
-network_data.decoder_activation = None#[tf.nn.tanh] * 2
+network_data.num_decoder_bw_units = [128, 128]
+network_data.num_decoder_fw_units = [128, 128]
+network_data.decoder_activation = [tf.nn.sigmoid, tf.nn.sigmoid]
 network_data.decoder_regularizer = 0.2
-network_data.decoder_output_sizes = [80, 200]
+network_data.decoder_output_sizes = [128, 256]
 network_data.decoder_out_activation = None
 
 network_data.encoding_features = 40
@@ -50,7 +50,7 @@ encoder_decoder.create_graph()
 
 test_database = Database.fromFile(project_data.TEST_ENCODER_DATABASE_FILE, project_data)
 test_feats, _ = test_database.to_set()
-test_feats = test_feats[0:100]
+test_feats = test_feats[0:1]
 
 encoder_decoder.train(
     input_seq=test_feats,
@@ -60,8 +60,8 @@ encoder_decoder.train(
     save_freq=10,
     use_tensorboard=True,
     tensorboard_freq=5,
-    training_epochs=5,
-    batch_size=20
+    training_epochs=30,
+    batch_size=5
 )
 
 out = encoder_decoder.predict(test_feats[0])
@@ -70,10 +70,10 @@ plt.imshow(out, cmap='hot', interpolation='nearest')
 f2 = plt.figure(1)
 plt.imshow(test_feats[0], cmap='hot', interpolation='nearest')
 
-out = encoder_decoder.predict(test_feats[1])
-f3 = plt.figure(2)
-plt.imshow(out, cmap='hot', interpolation='nearest')
-f4 = plt.figure(3)
-plt.imshow(test_feats[1], cmap='hot', interpolation='nearest')
+# out = encoder_decoder.predict(test_feats[1])
+# f3 = plt.figure(2)
+# plt.imshow(out, cmap='hot', interpolation='nearest')
+# f4 = plt.figure(3)
+# plt.imshow(test_feats[1], cmap='hot', interpolation='nearest')
 
 plt.show()
