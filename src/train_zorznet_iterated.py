fd0dc14dd778c630187cc56c4e216112aa0e6899
nicozorza <tecnico2011>
2018-10-28 15:52:39 -0300

    WIP: Add iterated ctc. Still not working. Tensor lengths error

---
+++ src/train_zorznet_iterated.py
@@ -0,0 +1,82 @@
+import tensorflow as tf
+from tensorflow.contrib.layers import l2_regularizer
+from src.neural_network.ZorzNetIterated.ZorzNetIteratedData import ZorzNetIteratedData
+from src.neural_network.ZorzNetIterated.ZorzNetIterated import ZorzNetIterated
+from src.neural_network.data_conversion import indexToStr
+from src.utils.Database import Database
+from src.utils.ProjectData import ProjectData
+
+###########################################################################################################
+# Load project data
+project_data = ProjectData()
+
+network_data = ZorzNetIteratedData()
+network_data.model_path = project_data.ZORZNET_ITERATED_MODEL_PATH
+network_data.checkpoint_path = project_data.ZORZNET_ITERATED_CHECKPOINT_PATH
+network_data.tensorboard_path = project_data.ZORZNET_ITERATED_TENSORBOARD_PATH
+
+network_data.num_classes = ord('z') - ord('a') + 1 + 1 + 1 + 1
+network_data.num_features = 26
+
+network_data.num_input_dense_layers = 1
+network_data.num_input_dense_units = [160]
+network_data.input_dense_activations = [tf.nn.tanh] * network_data.num_input_dense_layers
+network_data.input_batch_normalization = True
+
+network_data.is_bidirectional = True
+# network_data.num_cell_units = [250]
+# network_data.cell_activation = [tf.nn.tanh]
+network_data.num_fw_cell_units = [250]
+network_data.num_bw_cell_units = [110]
+network_data.cell_fw_activation = [tf.nn.tanh]
+network_data.cell_bw_activation = [tf.nn.tanh]
+network_data.rnn_regularizer = 0.3
+network_data.rnn_output_sizes = None
+
+network_data.num_dense_layers = 2
+network_data.num_dense_units = [75, 180]
+network_data.dense_activations = [tf.nn.relu] * network_data.num_dense_layers
+network_data.dense_regularizer = 0.3
+network_data.dense_batch_normalization = True
+
+network_data.use_dropout = True
+network_data.keep_dropout_input = [0.5]
+network_data.keep_dropout_output = [0.5, 0.5]
+
+network_data.decoder_function = tf.nn.ctc_greedy_decoder
+
+network_data.learning_rate = 0.001
+network_data.adam_epsilon = 0.0001
+network_data.optimizer = tf.train.AdamOptimizer(learning_rate=network_data.learning_rate,
+                                                epsilon=network_data.adam_epsilon)
+###########################################################################################################
+
+network = ZorzNetIterated(network_data)
+network.create_graph()
+
+train_database = Database.fromFile(project_data.TRAIN_DATABASE_FILE, project_data)
+test_database = Database.fromFile(project_data.TEST_DATABASE_FILE, project_data)
+
+train_feats, train_labels = train_database.to_set()
+train_feats = train_feats[0:10]
+train_labels = train_labels[0:10]
+test_feats, test_labels = test_database.to_set()
+
+network.train(
+    train_features=train_feats,
+    train_labels=train_labels,
+    restore_run=True,
+    save_partial=True,
+    save_freq=10,
+    use_tensorboard=True,
+    tensorboard_freq=5,
+    training_epochs=50,
+    batch_size=4
+)
+
+# network.validate(test_feats, test_labels, show_partial=False, batch_size=20)
+
+
+for i in range(2):     # len(val_feats)):
+    print('Predicted: {}'.format(network.predict(test_feats[i])))
+    print('Target: {}'.format(indexToStr(test_labels[i])))
