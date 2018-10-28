8d44dde7e7ad8425b15d409823db6aa0ae15c8a0
nicozorza <tecnico2011>
2018-10-28 15:51:55 -0300

    Add iterated ctc paths

--- src/utils/ProjectData.py
+++ src/utils/ProjectData.py
@@ -21,6 +21,10 @@ class ProjectData:
         self.ZORZNET_MODEL_PATH = self.OUT_DIR + '/zorznet/' + 'model/model'
         self.ZORZNET_TENSORBOARD_PATH = self.OUT_DIR + '/zorznet/' + 'tensorboard/'
 
+        self.ZORZNET_ITERATED_CHECKPOINT_PATH = self.OUT_DIR + '/zorznet_iter/' + 'checkpoint/'
+        self.ZORZNET_ITERATED_MODEL_PATH = self.OUT_DIR + '/zorznet_iter/' + 'model/model'
+        self.ZORZNET_ITERATED_TENSORBOARD_PATH = self.OUT_DIR + '/zorznet_iter/' + 'tensorboard/'
+
         self.ENC_DEC_CHECKPOINT_PATH = self.OUT_DIR + '/enc_dec/' + 'checkpoint/'
         self.ENC_DEC_MODEL_PATH = self.OUT_DIR + '/enc_dec/' + 'model/model'
         self.ENC_DEC_TENSORBOARD_PATH = self.OUT_DIR + '/enc_dec/' + 'tensorboard/'
