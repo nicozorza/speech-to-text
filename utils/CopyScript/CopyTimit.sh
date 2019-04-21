08420a30037c0b678fdd05032b42965d82a2b7ef
nicozorza <tecnico2011>
2019-04-21 15:56:56 -0300

    Add source path as argument

--- utils/CopyScript/CopyTimit.sh
+++ utils/CopyScript/CopyTimit.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 # This scripts takes all files in the TIMIT database and copies them to another folder
-search_dir=/home/nicozorza/Escritorio/speech-to-text/data/wav/TIMIT
 results_dir=results
 
-mkdir "$results_dir"
-mkdir "$results_dir/TRAIN"
-mkdir "$results_dir/TEST"
+if [ "$#" -eq 1 ]
+then
+	mkdir "$results_dir"
+	mkdir "$results_dir/TRAIN"
+	mkdir "$results_dir/TEST"
 
-for data_dir in TEST TRAIN
-do
-	for d in $(find "$search_dir/$data_dir/" -maxdepth 2 -type d)
+	search_dir=$1
+	for data_dir in TEST TRAIN
 	do
-		for file in "$d"/*
+		for d in $(find "$search_dir/$data_dir/" -maxdepth 2 -type d)
 		do
-			extension="${file##*.}"
-			filename="${file%.*}"
-			# El -f11 depende del path search_dir (en algun momento lo automatizare)
-			folder="$(cut -d'/' -f11 <<<${filename})"
+			for file in "$d"/*
+			do
+				extension="${file##*.}"
+				filename="${file%.*}"
+				# El -f11 depende del path search_dir (en algun momento lo automatizare)
+				folder="$(cut -d'/' -f11 <<<${filename})"
 
-			cp "$file" "${filename}_${folder}.${extension}"
-			mv "${filename}_${folder}.${extension}" "$results_dir/$data_dir/"
-			echo "$0: '${file}'"
+				cp "$file" "${filename}_${folder}.${extension}"
+				mv "${filename}_${folder}.${extension}" "$results_dir/$data_dir/"
+				echo "$0: '${file}'"
+			done
 		done
 	done
-done
+else
+	echo "Not enough arguments"
+	echo "Usage: ./CopyTimit.sh <source_dir>"
+	exit 1
+fi
