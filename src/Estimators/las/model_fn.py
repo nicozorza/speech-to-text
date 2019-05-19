Fix batch normalization

src/Estimators/las/model_fn.py
@@ -32,6 +32,7 @@ def model_fn(features,
                                           name='dense_layer_1',
                                           activation_list=params['dense_activations_1'],
                                           use_batch_normalization=params['batch_normalization_1'],
+                                          batch_normalization_trainable=params['batch_normalization_trainable_1'],
                                           train_ph=mode == tf.estimator.ModeKeys.TRAIN,
                                           use_tensorboard=True,
                                           keep_prob_list=params['keep_prob_1'],
@@ -59,6 +60,7 @@ def model_fn(features,
                                            name='dense_layer_2',
                                            activation_list=params['dense_activations_2'],
                                            use_batch_normalization=params['batch_normalization_2'],
+                                           batch_normalization_trainable=params['batch_normalization_trainable_2'],
                                            train_ph=mode == tf.estimator.ModeKeys.TRAIN,
                                            use_tensorboard=True,
                                            keep_prob_list=params['keep_prob_2'],
@@ -237,13 +239,15 @@ def model_fn(features,
         else:
             optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 
-        if params['clip_gradient'] != 0:
-            grads = tf.gradients(loss, tf.trainable_variables())
-            grads, _ = tf.clip_by_global_norm(grads, params['clip_gradient'])  # gradient clipping
-            grads_and_vars = list(zip(grads, tf.trainable_variables()))
-            train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
-        else:
-            train_op = optimizer.minimize(loss, global_step=global_step)
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            if params['clip_gradient'] != 0:
+                grads = tf.gradients(loss, tf.trainable_variables())
+                grads, _ = tf.clip_by_global_norm(grads, params['clip_gradient'])  # gradient clipping
+                grads_and_vars = list(zip(grads, tf.trainable_variables()))
+                train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+            else:
+                train_op = optimizer.minimize(loss, global_step=global_step)
 
     logging_hook = tf.train.LoggingTensorHook(
         tensors={

src/Estimators/las/train_las_estimator.py
@@ -25,11 +25,12 @@ network_data.eos_id = LASLabel.EOS_INDEX
 
 network_data.beam_width = 0
 
-network_data.num_dense_layers_1 = 1
-network_data.num_units_1 = [400]
-network_data.dense_activations_1 = [None] * network_data.num_dense_layers_1
+network_data.num_dense_layers_1 = 2
+network_data.num_units_1 = [400] * network_data.num_dense_layers_1
+network_data.dense_activations_1 = [tf.nn.relu] * network_data.num_dense_layers_1
 network_data.batch_normalization_1 = True
-network_data.keep_prob_1 = [0.8] * network_data.num_dense_layers_1
+network_data.batch_normalization_trainable_1 = True
+network_data.keep_prob_1 = [0.9] * network_data.num_dense_layers_1
 network_data.kernel_init_1 = [tf.truncated_normal_initializer(mean=0, stddev=0.1)] * network_data.num_dense_layers_1
 network_data.bias_init_1 = [tf.zeros_initializer()] * network_data.num_dense_layers_1
 
@@ -39,19 +40,20 @@ network_data.listener_activation_list = [tf.nn.tanh] * network_data.listener_num
 network_data.listener_keep_prob_list = [0.8] * network_data.listener_num_layers
 
 network_data.num_dense_layers_2 = 0
-network_data.num_units_2 = [200]
-network_data.dense_activations_2 = [tf.nn.relu] * network_data.num_dense_layers_2
+network_data.num_units_2 = [256]
+network_data.dense_activations_2 = [tf.nn.tanh] * network_data.num_dense_layers_2
 network_data.batch_normalization_2 = True
+network_data.batch_normalization_trainable_2 = False    # No funciona trainable=True luego de una RNN
 network_data.keep_prob_2 = [0.9] * network_data.num_dense_layers_2
 network_data.kernel_init_2 = [tf.truncated_normal_initializer(mean=0, stddev=0.1)] * network_data.num_dense_layers_2
 network_data.bias_init_2 = [tf.zeros_initializer()] * network_data.num_dense_layers_2
 
-network_data.attention_type = 'bahdanau'       # 'luong', 'bahdanau'
+network_data.attention_type = 'luong'       # 'luong', 'bahdanau'
 network_data.attention_num_layers = 1
 network_data.attention_size = None
 network_data.attention_units = 256
 network_data.attention_activation = tf.nn.tanh
-network_data.attention_keep_prob = 0.8
+network_data.attention_keep_prob = 0.9
 
 network_data.kernel_regularizer = 0.0
 network_data.sampling_probability = 0.2
@@ -68,19 +70,21 @@ network_data.momentum = None
 pprint.pprint(network_data.as_dict())
 # -------------------------------------------------------------------------------------------------------------------- #
 
-train_flag = True
-validate_flag = True
+train_flag = False
+validate_flag = False
 test_flag = True
+save_predictions = False
 
-restore_run = False
+restore_run = True
 model_dir = 'out/las_net/estimator/'
 
 train_files = ['data/train_database.tfrecords']
 validate_files = ['data/train_database.tfrecords']
-test_files = ['data/train_database.tfrecords']
+test_files = ['data/test_database.tfrecords']
+save_predictions_files = ['data/test_database.tfrecords']
 
 train_batch_size = 1
-train_epochs = 500
+train_epochs = 1
 
 validate_batch_size = 1
 
@@ -96,7 +100,7 @@ config = tf.estimator.RunConfig(
     model_dir=model_dir,
     save_checkpoints_steps=50,
     save_summary_steps=50,
-    log_step_count_steps=50)
+    log_step_count_steps=1)
 
 
 model = tf.estimator.Estimator(
@@ -149,12 +153,33 @@ if test_flag:
     count = 0
     for item in predictions:
         count += 1
-        # print(count)
-        if count >= 20:
+        if count >= 2:
             break
         pred = item['sample_ids']
-        # print(item['target_truth'])
-        # print("Target: " + LASLabel.from_index(item['target_truth']))
-        print("Predicted: " + LASLabel.from_index(pred))
-        print('')
+
+        a = LASLabel.from_index(pred)
+        print(str(count) + ' - ' + a)
+
+if save_predictions:
+    predictions = model.predict(
+        input_fn=lambda: data_input_fn(
+            filenames=save_predictions_files,
+            batch_size=1,
+            parse_fn=Database.tfrecord_parse_dense_fn,
+            shuffle_buffer=1,
+            num_features=network_data.num_features,
+            eos_id=LASLabel.EOS_INDEX,
+            sos_id=LASLabel.SOS_INDEX
+        )
+    )
+    count = 0
+    f = open("las_predictions.txt", "w")
+    for item in predictions:
+        count += 1
+
+        pred = item['sample_ids']
+        a = LASLabel.from_index(pred)
+        print(str(count) + ' - ' + a)
+        f.write(a + '\n')
+    f.close()
 

src/Estimators/zorznet/train_zorznet_estimator.py
@@ -16,19 +16,22 @@ network_data.model_path = project_data.ZORZNET_MODEL_PATH
 network_data.checkpoint_path = project_data.ZORZNET_CHECKPOINT_PATH
 network_data.tensorboard_path = project_data.ZORZNET_TENSORBOARD_PATH
 
+
+
 network_data.num_classes = ClassicLabel.num_classes - 1
 network_data.num_features = 494
 
 network_data.num_dense_layers_1 = 1
-network_data.num_units_1 = [400]
+network_data.num_units_1 = [400] * network_data.num_dense_layers_1
 network_data.dense_activations_1 = [tf.nn.relu] * network_data.num_dense_layers_1
 network_data.batch_normalization_1 = True
-network_data.keep_prob_1 = [0.9]
+network_data.batch_normalization_trainable_1 = True
+network_data.keep_prob_1 = [0.6] * network_data.num_dense_layers_1
 network_data.kernel_init_1 = [tf.truncated_normal_initializer(mean=0, stddev=0.1)] * network_data.num_dense_layers_1
 network_data.bias_init_1 = [tf.zeros_initializer()] * network_data.num_dense_layers_1
 
 network_data.is_bidirectional = True
-network_data.num_cell_units = [256, 256]
+network_data.num_cell_units = [512] * 2
 network_data.cell_activation = [tf.nn.tanh] * 2
 network_data.keep_prob_rnn = [0.8] * 2
 
@@ -36,11 +39,12 @@ network_data.num_dense_layers_2 = 1
 network_data.num_units_2 = [150, 100]
 network_data.dense_activations_2 = [tf.nn.relu] * network_data.num_dense_layers_2
 network_data.batch_normalization_2 = True
-network_data.keep_prob_2 = [0.9, 0.9]
+network_data.batch_normalization_trainable_2 = False    # No funciona trainable=True luego de una RNN
+network_data.keep_prob_2 = [0.6, 0.6]
 network_data.kernel_init_2 = [tf.truncated_normal_initializer(mean=0, stddev=0.1)] * network_data.num_dense_layers_2
 network_data.bias_init_2 = [tf.zeros_initializer()] * network_data.num_dense_layers_2
 
-network_data.dense_regularizer = 0.0
+network_data.dense_regularizer = 0.5
 network_data.rnn_regularizer = 0.0
 
 network_data.beam_width = 0    # 0 -> greedy_decoder, >0 -> beam_search
@@ -48,7 +52,7 @@ network_data.beam_width = 0    # 0 -> greedy_decoder, >0 -> beam_search
 network_data.learning_rate = 0.001
 network_data.use_learning_rate_decay = True
 network_data.learning_rate_decay_steps = 1000
-network_data.learning_rate_decay = 0.99
+network_data.learning_rate_decay = 0.98
 
 network_data.clip_gradient = 5
 network_data.optimizer = 'adam'      # 'rms', 'adam', 'momentum', 'sgd'
@@ -57,18 +61,20 @@ network_data.momentum = None
 # -------------------------------------------------------------------------------------------------------------------- #
 
 train_flag = True
-validate_flag = True
+validate_flag = False
 test_flag = True
+save_predictions = False
 
-restore_run = False
+restore_run = True
 model_dir = 'out/zorznet/estimator/'
 
 train_files = ['data/train_database.tfrecords']
-validate_files = ['data/train_database.tfrecords']
-test_files = ['data/train_database.tfrecords']
+validate_files = ['data/test_database_1.tfrecords', 'data/test_database_2.tfrecords']
+test_files = ['data/test_database.tfrecords']#, 'data/test_database_2.tfrecords']
+save_predictions_files = ['data/test_database.tfrecords']
 
 train_batch_size = 1
-train_epochs = 100
+train_epochs = 1
 
 validate_batch_size = 1
 
@@ -126,8 +132,25 @@ if test_flag:
             shuffle_buffer=1,
             num_features=network_data.num_features),
     )
-    # print(predictions)
+
     for item in predictions:
-        print(item)
-        print(ClassicLabel.from_index(item))
-        break
+        a = ClassicLabel.from_index(item)
+        print(a)
+
+if save_predictions:
+    predictions = model.predict(
+        input_fn=lambda: data_input_fn(
+            filenames=save_predictions_files,
+            batch_size=1,
+            parse_fn=Database.tfrecord_parse_dense_fn,
+            shuffle_buffer=1,
+            num_features=network_data.num_features),
+    )
+    count = 0
+    f = open("zorznet_predictions.txt", "w")
+    for item in predictions:
+        count += 1
+        a = ClassicLabel.from_index(item)
+        print(str(count) + ' - ' + a)
+        f.write(a + '\n')
+    f.close()

src/neural_network/network_utils/basic.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 
 
 def dense_layer(input_ph, num_units: int, name: str, activation=None,
-                use_batch_normalization: bool = True, train_ph: bool = True,
-                use_tensorboard: bool = True, keep_prob: float = 0,
+                use_batch_normalization: bool = True, batch_normalization_trainable: bool = False,
+                train_ph: bool = True, use_tensorboard: bool = True, keep_prob: float = 0,
                 tensorboard_scope: str = None,
                 kernel_initializer=None,
                 bias_initializer=None):
@@ -16,18 +16,25 @@ def dense_layer(input_ph, num_units: int, name: str, activation=None,
     out_ph = tf.layers.dense(
         inputs=input_ph,
         units=num_units,
-        activation=activation,
+        activation=None,
         name=name,
         kernel_initializer=kernel_initializer,
         bias_initializer=bias_initializer
     )
     if use_batch_normalization:
-        out_ph = tf.layers.batch_normalization(out_ph, name=name+"_batch_norm")
+        out_ph = tf.layers.batch_normalization(out_ph,
+                                               name=name+"_batch_norm",
+                                               training=train_ph,
+                                               trainable=batch_normalization_trainable)
+    if activation is not None:
+        out_ph = activation(out_ph)
+
     if keep_prob != 1:
         out_ph = tf.layers.dropout(out_ph,
                                    1 - keep_prob,
                                    training=train_ph,
                                    name=name+'_dropout')
+
     if use_tensorboard:
         if tensorboard_scope is None:
             tb_name = name
@@ -39,8 +46,8 @@ def dense_layer(input_ph, num_units: int, name: str, activation=None,
 
 
 def dense_multilayer(input_ph, num_layers: int, num_units: List[int], name: str, activation_list,
-                     use_batch_normalization: bool = True, train_ph: bool = True,
-                     use_tensorboard: bool = True, keep_prob_list: List[float] = 0,
+                     use_batch_normalization: bool = True, batch_normalization_trainable: bool = False,
+                     train_ph: bool = True, use_tensorboard: bool = True, keep_prob_list: List[float] = 0,
                      tensorboard_scope: str = None,
                      kernel_initializers=None,
                      bias_initializers=None):
@@ -68,5 +75,6 @@ def dense_multilayer(input_ph, num_layers: int, num_units: List[int], name: str,
                                keep_prob=keep_prob_list[_],
                                tensorboard_scope=tensorboard_scope,
                                kernel_initializer=kernel_initializers[_],
-                               bias_initializer=bias_initializers[_])
+                               bias_initializer=bias_initializers[_],
+                               batch_normalization_trainable=batch_normalization_trainable)
     return input_ph