Fix update_ops for batch normalization

src/Estimators/las/model_fn.py
@@ -76,8 +76,7 @@ def model_fn(features,
                                            keep_prob_list=params['keep_prob_2'],
                                            kernel_initializers=params['kernel_init_2'],
                                            bias_initializers=params['bias_init_2'],
-                                           tensorboard_scope='dense_layer_2',
-                                           batch_normalization_training=True)
+                                           tensorboard_scope='dense_layer_2')
 
     with tf.variable_scope('tile_batch'):
         batch_size = tf.shape(listener_output)[0]
@@ -250,15 +249,14 @@ def model_fn(features,
         else:
             optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-        with tf.control_dependencies(update_ops):
-            if params['clip_gradient'] != 0:
-                grads = tf.gradients(loss, tf.trainable_variables())
-                grads, _ = tf.clip_by_global_norm(grads, params['clip_gradient'])  # gradient clipping
-                grads_and_vars = list(zip(grads, tf.trainable_variables()))
-                train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
-            else:
-                train_op = optimizer.minimize(loss, global_step=global_step)
+        loss = tf.tuple([loss], control_inputs=tf.get_collection(tf.GraphKeys.UPDATE_OPS))[0]
+        if params['clip_gradient'] != 0:
+            grads = tf.gradients(loss, tf.trainable_variables())
+            grads, _ = tf.clip_by_global_norm(grads, params['clip_gradient'])
+            grads_and_vars = list(zip(grads, tf.trainable_variables()))
+            train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+        else:
+            train_op = optimizer.minimize(loss, global_step=global_step)
 
     logging_hook = tf.train.LoggingTensorHook(
         tensors={

src/Estimators/zorznet/model_fn.py
@@ -89,7 +89,8 @@ def model_fn(features, labels, mode, config, params):
                                        kernel_initializers=params['kernel_init_2'],
                                        bias_initializers=params['bias_init_2'],
                                        tensorboard_scope='dense_layer_2',
-                                       batch_normalization_training=True)
+                                       # batch_normalization_training=True
+                                       )
 
     with tf.name_scope("dense_output"):
         dense_output_no_activation = dense_layer(input_ph=rnn_outputs,
@@ -176,15 +177,14 @@ def model_fn(features, labels, mode, config, params):
         else:
             optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 
-        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-        with tf.control_dependencies(update_ops):
-            if params['clip_gradient'] != 0:
-                grads = tf.gradients(loss, tf.trainable_variables())
-                grads, _ = tf.clip_by_global_norm(grads, params['clip_gradient'])
-                grads_and_vars = list(zip(grads, tf.trainable_variables()))
-                train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
-            else:
-                train_op = optimizer.minimize(loss, global_step=global_step)
+        loss = tf.tuple([loss], control_inputs=tf.get_collection(tf.GraphKeys.UPDATE_OPS))[0]
+        if params['clip_gradient'] != 0:
+            grads = tf.gradients(loss, tf.trainable_variables())
+            grads, _ = tf.clip_by_global_norm(grads, params['clip_gradient'])
+            grads_and_vars = list(zip(grads, tf.trainable_variables()))
+            train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+        else:
+            train_op = optimizer.minimize(loss, global_step=global_step)
 
         train_logging_hook = tf.train.LoggingTensorHook(
             tensors={

src/neural_network/network_utils/basic.py
@@ -8,7 +8,7 @@ def dense_layer(input_ph, num_units: int, name: str, activation=None,
                 train_ph: bool = True, use_tensorboard: bool = True, keep_prob: float = 0,
                 tensorboard_scope: str = None,
                 kernel_initializer=None,
-                bias_initializer=None, batch_normalization_training=None):
+                bias_initializer=None):
 
     out_ph = tf.layers.dense(
         inputs=input_ph,
@@ -19,10 +19,9 @@ def dense_layer(input_ph, num_units: int, name: str, activation=None,
         bias_initializer=bias_initializer
     )
     if use_batch_normalization:
-        bn_training = batch_normalization_training if batch_normalization_training is not None else train_ph
         out_ph = tf.layers.batch_normalization(out_ph,
                                                name=name + "_batch_norm",
-                                               training=bn_training,
+                                               training=train_ph,
                                                trainable=batch_normalization_trainable)
 
     if keep_prob != 1:
@@ -46,7 +45,7 @@ def dense_multilayer(input_ph, num_layers: int, num_units: List[int], name: str,
                      train_ph: bool = True, use_tensorboard: bool = True, keep_prob_list: List[float] = 0,
                      tensorboard_scope: str = None,
                      kernel_initializers=None,
-                     bias_initializers=None, batch_normalization_training=None):
+                     bias_initializers=None):
 
     if activation_list is None:
         activation_list = [None] * num_layers
@@ -72,6 +71,5 @@ def dense_multilayer(input_ph, num_layers: int, num_units: List[int], name: str,
                                tensorboard_scope=tensorboard_scope,
                                kernel_initializer=kernel_initializers[_],
                                bias_initializer=bias_initializers[_],
-                               batch_normalization_trainable=batch_normalization_trainable,
-                               batch_normalization_training=batch_normalization_training)
+                               batch_normalization_trainable=batch_normalization_trainable)
     return input_ph